fb660fe2d0c7f9e3c816e0547756d12eadf813c5
Alan Dono <alannicolasdono@gmail.com>
2023-05-13 02:17:41 -0300

    Creamos la vista para la administracion de libros (solo Crear)

---
+++ TP6DonoAlan/TP6DonoAlan/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace TP6DonoAlan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.lblGenero = new System.Windows.Forms.Label();
+            this.lblPaguina = new System.Windows.Forms.Label();
+            this.lblImportado = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.cbGenero = new System.Windows.Forms.ComboBox();
+            this.txtPaginas = new System.Windows.Forms.TextBox();
+            this.chkBoxSi = new System.Windows.Forms.CheckBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtLista = new System.Windows.Forms.TextBox();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl.Location = new System.Drawing.Point(-3, -1);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(761, 628);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 34);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(753, 590);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ingreso";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnInsert);
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.chkBoxSi);
+            this.groupBox1.Controls.Add(this.txtPaginas);
+            this.groupBox1.Controls.Add(this.cbGenero);
+            this.groupBox1.Controls.Add(this.txtAutor);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.lblPrecio);
+            this.groupBox1.Controls.Add(this.lblImportado);
+            this.groupBox1.Controls.Add(this.lblPaguina);
+            this.groupBox1.Controls.Add(this.lblGenero);
+            this.groupBox1.Controls.Add(this.lblAutor);
+            this.groupBox1.Controls.Add(this.lblName);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(747, 588);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingresar Libro";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtLista);
+            this.tabPage2.Location = new System.Drawing.Point(4, 34);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(753, 590);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lista";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(75, 62);
+            this.lblName.Margin = new System.Windows.Forms.Padding(10);
+            this.lblName.Name = "lblName";
+            this.lblName.Padding = new System.Windows.Forms.Padding(5);
+            this.lblName.Size = new System.Drawing.Size(111, 39);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nombre";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAutor.Location = new System.Drawing.Point(108, 120);
+            this.lblAutor.Margin = new System.Windows.Forms.Padding(10);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Padding = new System.Windows.Forms.Padding(5);
+            this.lblAutor.Size = new System.Drawing.Size(79, 39);
+            this.lblAutor.TabIndex = 1;
+            this.lblAutor.Text = "Autor";
+            this.lblAutor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGenero
+            // 
+            this.lblGenero.AutoSize = true;
+            this.lblGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenero.Location = new System.Drawing.Point(82, 180);
+            this.lblGenero.Margin = new System.Windows.Forms.Padding(10);
+            this.lblGenero.Name = "lblGenero";
+            this.lblGenero.Padding = new System.Windows.Forms.Padding(5);
+            this.lblGenero.Size = new System.Drawing.Size(104, 39);
+            this.lblGenero.TabIndex = 2;
+            this.lblGenero.Text = "Genero";
+            this.lblGenero.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPaguina
+            // 
+            this.lblPaguina.AutoSize = true;
+            this.lblPaguina.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaguina.Location = new System.Drawing.Point(81, 241);
+            this.lblPaguina.Margin = new System.Windows.Forms.Padding(10);
+            this.lblPaguina.Name = "lblPaguina";
+            this.lblPaguina.Padding = new System.Windows.Forms.Padding(5);
+            this.lblPaguina.Size = new System.Drawing.Size(104, 39);
+            this.lblPaguina.TabIndex = 2;
+            this.lblPaguina.Text = "Pagina ";
+            this.lblPaguina.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblImportado
+            // 
+            this.lblImportado.AutoSize = true;
+            this.lblImportado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImportado.Location = new System.Drawing.Point(59, 300);
+            this.lblImportado.Margin = new System.Windows.Forms.Padding(10);
+            this.lblImportado.Name = "lblImportado";
+            this.lblImportado.Padding = new System.Windows.Forms.Padding(5);
+            this.lblImportado.Size = new System.Drawing.Size(132, 39);
+            this.lblImportado.TabIndex = 3;
+            this.lblImportado.Text = "Importado";
+            this.lblImportado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(93, 359);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(10);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Padding = new System.Windows.Forms.Padding(5);
+            this.lblPrecio.Size = new System.Drawing.Size(93, 39);
+            this.lblPrecio.TabIndex = 4;
+            this.lblPrecio.Text = "Precio";
+            this.lblPrecio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(205, 64);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(260, 34);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAutor.Location = new System.Drawing.Point(205, 125);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(260, 34);
+            this.txtAutor.TabIndex = 6;
+            // 
+            // cbGenero
+            // 
+            this.cbGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbGenero.FormattingEnabled = true;
+            this.cbGenero.Location = new System.Drawing.Point(205, 182);
+            this.cbGenero.Name = "cbGenero";
+            this.cbGenero.Size = new System.Drawing.Size(260, 37);
+            this.cbGenero.TabIndex = 7;
+            // 
+            // txtPaginas
+            // 
+            this.txtPaginas.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPaginas.Location = new System.Drawing.Point(205, 243);
+            this.txtPaginas.Name = "txtPaginas";
+            this.txtPaginas.Size = new System.Drawing.Size(119, 34);
+            this.txtPaginas.TabIndex = 8;
+            // 
+            // chkBoxSi
+            // 
+            this.chkBoxSi.AutoSize = true;
+            this.chkBoxSi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBoxSi.Location = new System.Drawing.Point(205, 304);
+            this.chkBoxSi.Name = "chkBoxSi";
+            this.chkBoxSi.Size = new System.Drawing.Size(57, 33);
+            this.chkBoxSi.TabIndex = 9;
+            this.chkBoxSi.Text = "Si";
+            this.chkBoxSi.UseVisualStyleBackColor = true;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(205, 361);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(119, 34);
+            this.txtPrecio.TabIndex = 10;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(168, 486);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(132, 37);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(427, 486);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(132, 37);
+            this.btnInsert.TabIndex = 12;
+            this.btnInsert.Text = "Ingresar";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // txtLista
+            // 
+            this.txtLista.Location = new System.Drawing.Point(37, 82);
+            this.txtLista.Multiline = true;
+            this.txtLista.Name = "txtLista";
+            this.txtLista.Size = new System.Drawing.Size(668, 407);
+            this.txtLista.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 625);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
+            this.Text = "Gestion de libros";
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblImportado;
+        private System.Windows.Forms.Label lblPaguina;
+        private System.Windows.Forms.Label lblGenero;
+        private System.Windows.Forms.Label lblAutor;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.CheckBox chkBoxSi;
+        private System.Windows.Forms.TextBox txtPaginas;
+        private System.Windows.Forms.ComboBox cbGenero;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtLista;
+    }
+}
+
